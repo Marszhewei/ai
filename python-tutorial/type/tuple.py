a88a9493252374a52ce612d9855c6250eb48ce89
fetch150zy <fetch150zy@pop-os.localdomain>
2022-10-19 11:36:22 +0800

    tutorial code

---
+++ python-tutorial/type/tuple.py
@@ -0,0 +1,15 @@
+stu_name = ('jack', 'mars', 'zwei')
+print(stu_name)
+print(len(stu_name))
+
+# you can't change elements
+# stu_name[1] = 'jhon'
+# print(stu_name)
+
+# empty tuple and one element tuple
+empty_tuple = ()
+print(empty_tuple)
+print(len(empty_tuple))
+one_tuple = (1)
+print(one_tuple)
+# print(len(one_tuple)) can't get length of one element tuple
